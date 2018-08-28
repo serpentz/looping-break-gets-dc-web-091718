e10d671b48acb27dff46def2918846b8a6abe96e
Absalom Cherinet <absalomcherinet@gmail.com>
2018-08-28 19:34:19 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,7 +2,7 @@
 def levitation_quiz
   answer = " "
 
-  until answer != "Wingardium Leviosa"
+  while answer != "Wingardium Leviosa"
       puts "What is the spell that enacts levitation?"
       answer = gets
   end
